297389c1e0a9db4e41133bb7e178c3220bee43d6
Guilherme Carbonari Boneti <guilherme.carbonari1903@gmail.com>
2023-01-25 11:58:30 -0300

    begin seriabilidade

--- escalona.c
+++ escalona.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM_BUFFER 20
 
 typedef struct transacao {
     int tempo_chegada;
@@ -9,39 +10,109 @@ typedef struct transacao {
 
 } transacao;
 
-void readList(transacao **lista, int *tam) {
+typedef struct node {
+    int index;
+    int *points_to;
+} node;
+
+void readList(transacao *list, int *tam) {
     int i;
-    for (i=0; ;i++) {
-        if (scanf("%d", &lista[i]->tempo_chegada) == EOF)
-            break;
-        scanf("%d", &lista[i]->identificador);
-        scanf(" %c", &lista[i]->operacao);
-        scanf(" %c", &lista[i]->atributo);
-        tam++;
+    char line[TAM_BUFFER];
+    for (i=0; i<5;i++) {
+        fgets(line, TAM_BUFFER, stdin);
+        sscanf(line, "%d %d %c %c", &list[i].tempo_chegada, &list[i].identificador, &list[i].operacao, &list[i].atributo);
+        (*tam)++;
     }
+}
+
+void printList(transacao *list, int tam) {
+    int i;
+    for (i=0; i<tam; i++){
+        printf("%d", list[i].tempo_chegada);
+        printf(" - %d", list[i].identificador);
+        printf(" - %c", list[i].operacao);
+        printf(" - %c\n", list[i].atributo);
+    }
+}
+
+int newNode(node *list, int tam, int identificador) {
+    int i;
+    for(i=0; i<tam; i++) {
+        if (list[i].index == identificador)
+            return 0;
+    }
+    return 1;
+}
+
+int detectCycle() {
+    return 1;
+}
+
+void criaAresta(node *list, int Ti, int Tj, int tam) {
+    int i, j;
+    for (i=0; i<tam; i++) {
+        if (list[i].index == Ti) {
+            for (j=0; j<tam; j++) {
+                if (list[i].points_to[j] == 0)
+                    list[i].points_to[j] = Tj;
+            }
+        }
+    }
+}
+
+int seriabilidade(transacao *transacoes, int tam) {
+    int i, j, m;
+    node *list_nodes = (node*) malloc(tam*sizeof(node));
+    for(i=0; i<tam; i++)
+        list_nodes[i].points_to = (int*) calloc(tam,sizeof(int));
+
+     // cria um nó para cada T do escalonamento
+    for (i=0; i<tam; i++) {
+        if (newNode(list_nodes, tam, transacoes[i].identificador))
+            list_nodes[i].index = transacoes[i].identificador;
+    }
+
+    // cria aresta Ti -> Tj para cada r(x) em Tj depois de w(x) em Ti
+    for (i=0; i<tam; i++) {     // percorre os nós T
+        int Ti = list_nodes[i].index;
+        if (Ti == 0)
+            break;
+        
+        for (j=0; j<tam-1; j++) {     // percorre a lista de transacoes
+            if (transacoes[j].identificador==Ti && transacoes[j].operacao=='R') {
+                for (m=j+1; m<tam; m++) {
+                    if (transacoes[m].identificador != Ti && transacoes[m].operacao=='W' && transacoes[j].atributo == transacoes[m].atributo)
+                        criaAresta(list_nodes, Ti, transacoes[m].identificador, tam);
+                }
+            }
+        }
+    }
+
+    for (i=0;i<tam;i++) {
+        if (list_nodes[i].index==0)
+            break;
+        printf("T%d:\n", list_nodes[i].index);
+        for (j=0;j<tam;j++) {
+            printf("%d",  list_nodes[i].points_to[j]);
+        }
+        printf("\n");
+    }
+
+    if (detectCycle())    // se há ciclo no grafo
+        return 1;   // é serial
+    else
+        return 0;   // não é serial
 }
 
 int main() {
     int i, tam_list=0;
-    transacao **lista_transacoes = malloc (10 * sizeof(transacao*)) ;
+    transacao *lista_transacoes = malloc (TAM_BUFFER * sizeof(transacao)) ;
     readList(lista_transacoes, &tam_list);
+    if (seriabilidade(lista_transacoes, tam_list))
+        printf("É serial\n");
+    else
+        printf("Não é serial\n");
 
-    // for (i=0; ;i++) {
-    //     if (scanf("%d", &lista_transacoes[i].tempo_chegada) == EOF)
-    //         break;
-    //     scanf("%d", &lista_transacoes[i].identificador);
-    //     scanf(" %c", &lista_transacoes[i].operacao);
-    //     scanf(" %c", &lista_transacoes[i].atributo);
-    //     tam_list++;
-    // }
-
-
-    for (i=0; i<tam_list; i++){
-        printf("%d", lista_transacoes[i]->tempo_chegada);
-        printf(" - %d", lista_transacoes[i]->identificador);
-        printf(" - %c", lista_transacoes[i]->operacao);
-        printf(" - %c\n", lista_transacoes[i]->atributo);
-    }
-
+    printList(lista_transacoes, tam_list);
     return 0;
 }
